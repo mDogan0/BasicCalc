0b45129002699df9e747041aaf6c0ea6a4d19c4e
Muhammet Dogan <muhamtdgan@gmail.com>
2026-03-17 17:13:47 +0300

    Project finished.

--- BasicCalc/Form1.Designer.cs
+++ BasicCalc/Form1.Designer.cs
@@ -29,136 +29,152 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            lbl_number_one = new Label();
-            lbl_number_two = new Label();
-            lbl_sonuc = new Label();
-            txtSayi1 = new TextBox();
-            txtSayi2 = new TextBox();
-            txtSonuc = new TextBox();
-            btnArti = new Button();
-            btnEksi = new Button();
-            btnCarpi = new Button();
-            btnBol = new Button();
+            lblNumber1 = new Label();
+            lblNumber2 = new Label();
+            lbl_result = new Label();
+            txtNumber1 = new TextBox();
+            txtNumber2 = new TextBox();
+            txtResult = new TextBox();
+            btnPlus = new Button();
+            btnMinus = new Button();
+            btnCross = new Button();
+            btnDivide = new Button();
+            btnClear = new Button();
             SuspendLayout();
             // 
-            // lbl_number_one
+            // lblNumber1
             // 
-            lbl_number_one.AutoSize = true;
-            lbl_number_one.Font = new Font("Segoe UI", 15F);
-            lbl_number_one.Location = new Point(140, 66);
-            lbl_number_one.Name = "lbl_number_one";
-            lbl_number_one.Size = new Size(104, 28);
-            lbl_number_one.TabIndex = 0;
-            lbl_number_one.Text = "Number 1:";
+            lblNumber1.AutoSize = true;
+            lblNumber1.Font = new Font("Segoe UI", 15F);
+            lblNumber1.Location = new Point(140, 66);
+            lblNumber1.Name = "lblNumber1";
+            lblNumber1.Size = new Size(104, 28);
+            lblNumber1.TabIndex = 0;
+            lblNumber1.Text = "Number 1:";
             // 
-            // lbl_number_two
+            // lblNumber2
             // 
-            lbl_number_two.AutoSize = true;
-            lbl_number_two.Font = new Font("Segoe UI", 15F);
-            lbl_number_two.Location = new Point(140, 112);
-            lbl_number_two.Name = "lbl_number_two";
-            lbl_number_two.Size = new Size(104, 28);
-            lbl_number_two.TabIndex = 1;
-            lbl_number_two.Text = "Number 2:";
+            lblNumber2.AutoSize = true;
+            lblNumber2.Font = new Font("Segoe UI", 15F);
+            lblNumber2.Location = new Point(140, 112);
+            lblNumber2.Name = "lblNumber2";
+            lblNumber2.Size = new Size(104, 28);
+            lblNumber2.TabIndex = 1;
+            lblNumber2.Text = "Number 2:";
             // 
-            // lbl_sonuc
+            // lbl_result
             // 
-            lbl_sonuc.AutoSize = true;
-            lbl_sonuc.Font = new Font("Segoe UI", 15F);
-            lbl_sonuc.Location = new Point(140, 259);
-            lbl_sonuc.Name = "lbl_sonuc";
-            lbl_sonuc.Size = new Size(75, 28);
-            lbl_sonuc.TabIndex = 2;
-            lbl_sonuc.Text = "Sonuç: ";
+            lbl_result.AutoSize = true;
+            lbl_result.Font = new Font("Segoe UI", 15F);
+            lbl_result.Location = new Point(140, 259);
+            lbl_result.Name = "lbl_result";
+            lbl_result.Size = new Size(68, 28);
+            lbl_result.TabIndex = 2;
+            lbl_result.Text = "Result:";
             // 
-            // txtSayi1
+            // txtNumber1
             // 
-            txtSayi1.Font = new Font("Segoe UI", 15F);
-            txtSayi1.Location = new Point(250, 66);
-            txtSayi1.Name = "txtSayi1";
-            txtSayi1.Size = new Size(176, 34);
-            txtSayi1.TabIndex = 3;
+            txtNumber1.Font = new Font("Segoe UI", 15F);
+            txtNumber1.Location = new Point(250, 66);
+            txtNumber1.Name = "txtNumber1";
+            txtNumber1.Size = new Size(176, 34);
+            txtNumber1.TabIndex = 0;
+            txtNumber1.TextChanged += txtNumber1_TextChanged;
             // 
-            // txtSayi2
+            // txtNumber2
             // 
-            txtSayi2.Font = new Font("Segoe UI", 15F);
-            txtSayi2.Location = new Point(250, 112);
-            txtSayi2.Name = "txtSayi2";
-            txtSayi2.Size = new Size(176, 34);
-            txtSayi2.TabIndex = 4;
+            txtNumber2.Font = new Font("Segoe UI", 15F);
+            txtNumber2.Location = new Point(250, 112);
+            txtNumber2.Name = "txtNumber2";
+            txtNumber2.Size = new Size(176, 34);
+            txtNumber2.TabIndex = 1;
+            txtNumber2.TextChanged += txtNumber2_TextChanged;
             // 
-            // txtSonuc
+            // txtResult
             // 
-            txtSonuc.Font = new Font("Segoe UI", 15F);
-            txtSonuc.Location = new Point(220, 259);
-            txtSonuc.Name = "txtSonuc";
-            txtSonuc.ReadOnly = true;
-            txtSonuc.Size = new Size(206, 34);
-            txtSonuc.TabIndex = 5;
+            txtResult.Font = new Font("Segoe UI", 15F);
+            txtResult.Location = new Point(250, 259);
+            txtResult.Name = "txtResult";
+            txtResult.ReadOnly = true;
+            txtResult.Size = new Size(176, 34);
+            txtResult.TabIndex = 6;
             // 
-            // btnArti
+            // btnPlus
             // 
-            btnArti.BackColor = SystemColors.Control;
-            btnArti.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            btnArti.Location = new Point(141, 173);
-            btnArti.Name = "btnArti";
-            btnArti.Size = new Size(50, 60);
-            btnArti.TabIndex = 6;
-            btnArti.Text = "+";
-            btnArti.UseVisualStyleBackColor = false;
-            btnArti.Click += btnArti_Click;
+            btnPlus.BackColor = SystemColors.Control;
+            btnPlus.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            btnPlus.Location = new Point(141, 173);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(50, 60);
+            btnPlus.TabIndex = 2;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = false;
+            btnPlus.Click += btnPlus_Click;
             // 
-            // btnEksi
+            // btnMinus
             // 
-            btnEksi.BackColor = SystemColors.Control;
-            btnEksi.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            btnEksi.Location = new Point(217, 173);
-            btnEksi.Name = "btnEksi";
-            btnEksi.Size = new Size(50, 60);
-            btnEksi.TabIndex = 7;
-            btnEksi.Text = "-";
-            btnEksi.UseVisualStyleBackColor = false;
-            btnEksi.Click += btnEksi_Click;
+            btnMinus.BackColor = SystemColors.Control;
+            btnMinus.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            btnMinus.Location = new Point(217, 173);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(50, 60);
+            btnMinus.TabIndex = 3;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = false;
+            btnMinus.Click += btnMinus_Click;
             // 
-            // btnCarpi
+            // btnCross
             // 
-            btnCarpi.BackColor = SystemColors.Control;
-            btnCarpi.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            btnCarpi.Location = new Point(295, 173);
-            btnCarpi.Name = "btnCarpi";
-            btnCarpi.Size = new Size(50, 60);
-            btnCarpi.TabIndex = 8;
-            btnCarpi.Text = "*";
-            btnCarpi.UseVisualStyleBackColor = false;
-            btnCarpi.Click += btnCarpi_Click;
+            btnCross.BackColor = SystemColors.Control;
+            btnCross.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            btnCross.Location = new Point(295, 173);
+            btnCross.Name = "btnCross";
+            btnCross.Size = new Size(50, 60);
+            btnCross.TabIndex = 4;
+            btnCross.Text = "*";
+            btnCross.UseVisualStyleBackColor = false;
+            btnCross.Click += btnCross_Click;
             // 
-            // btnBol
+            // btnDivide
             // 
-            btnBol.BackColor = SystemColors.Control;
-            btnBol.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            btnBol.Location = new Point(376, 173);
-            btnBol.Name = "btnBol";
-            btnBol.Size = new Size(50, 60);
-            btnBol.TabIndex = 9;
-            btnBol.Text = "/";
-            btnBol.UseVisualStyleBackColor = false;
-            btnBol.Click += btnBol_Click;
+            btnDivide.BackColor = SystemColors.Control;
+            btnDivide.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            btnDivide.Location = new Point(376, 173);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(50, 60);
+            btnDivide.TabIndex = 5;
+            btnDivide.Text = "/";
+            btnDivide.UseVisualStyleBackColor = false;
+            btnDivide.Click += btnDivide_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = Color.FromArgb(192, 255, 192);
+            btnClear.Location = new Point(364, 304);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(62, 34);
+            btnClear.TabIndex = 7;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
             ClientSize = new Size(556, 350);
-            Controls.Add(btnBol);
-            Controls.Add(btnCarpi);
-            Controls.Add(btnEksi);
-            Controls.Add(btnArti);
-            Controls.Add(txtSonuc);
-            Controls.Add(txtSayi2);
-            Controls.Add(txtSayi1);
-            Controls.Add(lbl_sonuc);
-            Controls.Add(lbl_number_two);
-            Controls.Add(lbl_number_one);
+            Controls.Add(btnClear);
+            Controls.Add(btnDivide);
+            Controls.Add(btnCross);
+            Controls.Add(btnMinus);
+            Controls.Add(btnPlus);
+            Controls.Add(txtResult);
+            Controls.Add(txtNumber2);
+            Controls.Add(txtNumber1);
+            Controls.Add(lbl_result);
+            Controls.Add(lblNumber2);
+            Controls.Add(lblNumber1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
@@ -169,15 +185,16 @@
 
         #endregion
 
-        private Label lbl_number_one;
-        private Label lbl_number_two;
-        private Label lbl_sonuc;
-        private TextBox txtSayi1;
-        private TextBox txtSayi2;
-        private TextBox txtSonuc;
-        private Button btnArti;
-        private Button btnEksi;
-        private Button btnCarpi;
-        private Button btnBol;
+        private Label lblNumber1;
+        private Label lblNumber2;
+        private Label lbl_result;
+        private TextBox txtNumber1;
+        private TextBox txtNumber2;
+        private TextBox txtResult;
+        private Button btnPlus;
+        private Button btnMinus;
+        private Button btnCross;
+        private Button btnDivide;
+        private Button btnClear;
     }
 }
--- BasicCalc/Form1.cs
+++ BasicCalc/Form1.cs
@@ -7,23 +7,98 @@ namespace BasicCalc
             InitializeComponent();
         }
 
-        private void btnArti_Click(object sender, EventArgs e)
+        private void btnPlus_Click(object sender, EventArgs e)
         {
-             
+            try
+            {
+
+                decimal number1 = decimal.Parse(txtNumber1.Text);
+                decimal number2 = decimal.Parse(txtNumber2.Text);
+                decimal result = number1 + number2;
+                txtResult.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir sayý giriniz!", "Giriþ Hatasý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            txtNumber1.Focus();
         }
-        private void btnEksi_Click(object sender, EventArgs e)
+
+        private void btnMinus_Click(object sender, EventArgs e)
         {
+            try
+            {
+
+                decimal number1 = decimal.Parse(txtNumber1.Text);
+                decimal number2 = decimal.Parse(txtNumber2.Text);
+                decimal result = number1 - number2;
+                txtResult.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir sayý giriniz!", "Giriþ Hatasý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            txtNumber1.Focus();
+        }
+        private void btnCross_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal number1 = decimal.Parse(txtNumber1.Text);
+                decimal number2 = decimal.Parse(txtNumber2.Text);
+                decimal result = number1 * number2;
+                txtResult.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir sayý giriniz!", "Giriþ Hatasý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            txtNumber1.Focus();
 
         }
-        private void btnCarpi_Click(object sender, EventArgs e)
+        private void btnDivide_Click(object sender, EventArgs e)
         {
 
+            try
+            {
+                decimal number1 = decimal.Parse(txtNumber1.Text);
+                decimal number2 = decimal.Parse(txtNumber2.Text);
+                if (number2 == 0)
+                {
+                    MessageBox.Show("Bir sayý 0'a bölünemez.");
+                    return;
+                }
+                decimal result = number1 / number2;
+                txtResult.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir sayý giriniz!", "Giriþ Hatasý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ýþlem sýrasýnda bir hata oluþtu. Tekrar deneyin.");
+            }
+            txtNumber1.Focus();
         }
-        private void btnBol_Click(object sender, EventArgs e)
+
+        private void btnClear_Click(object sender, EventArgs e)
         {
+            txtNumber1.Clear();
+            txtNumber2.Clear();
+            txtResult.Clear();
+        }
+
+        private void txtNumber1_TextChanged(object sender, EventArgs e)
+        {
+            txtResult.Clear();
 
         }
 
-        
+        private void txtNumber2_TextChanged(object sender, EventArgs e)
+        {
+            txtResult.Clear();
+
+        }
     }
 }
